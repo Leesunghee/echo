0b668c02ffeb4631a53778484320ef2524b95e2c
Leesunghee <realwunz@gmail.com>
2018-12-09 21:23:02 +0900

    using View Contoller

--- src/main/java/com/ready2die/config/WebMvcConfig.java
+++ src/main/java/com/ready2die/config/WebMvcConfig.java
@@ -2,7 +2,9 @@ package com.ready2die.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -14,5 +16,13 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.jsp();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/error").setViewName("error");
+        registry.addRedirectViewController("/bad_request", "/error");
+        registry.addStatusController("/detail", HttpStatus.BAD_REQUEST);
     }
 }
---
+++ web/WEB-INF/error.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: sunghee
+  Date: 09/12/2018
+  Time: 9:12 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Error</title>
+</head>
+<body>
+    <h2>Error</h2>
+</body>
+</html>
